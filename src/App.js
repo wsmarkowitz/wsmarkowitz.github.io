cc0f68c89f412ed774080d10c56e72ac7aee1ca4
Sam Markowitz <wmarkowitz@college.harvard.edu>
2022-07-30 03:17:22 -0400

    test commits

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   <Box backgroundColor={theme.main} height={'2000px'} color={theme.textColor}>
     <Box>ABOUT ME</Box>
     <Box>Samuel Markowitz is a driven game developer, currently working as an Associate Audio Programmer at Infinity Ward, developer of some of the most critically acclaimed entries in the Call of Duty franchise.</Box>
+    TEST TEST
   </Box>
   </ThemeProvider>
   )
